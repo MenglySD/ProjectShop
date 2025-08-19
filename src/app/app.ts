e9732d3956d8455eb1a5a14bfe9acef7f4f886f3
Admin <Admin@LAPTOP-TFUQNFTC>
2025-08-20 02:34:23 +0700

    firts commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { Footer } from './footer/footer';
+import { CartService } from './service/cart-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Footer ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'ClotheShop';
+
+export class App implements OnInit {
+    ngOnInit(): void {
+    initFlowbite();
+  }
+
+  constructor(public cartService: CartService) {
+
+  }
 }
